251bfa772b0e09cb176dfae8f846522eb9bbccf6
Oscar A <oscariasv@gmail.com>
2020-07-11 09:53:53 -0600

    cambios kali

--- models/Producto.php
+++ models/Producto.php
@@ -9,15 +9,15 @@ class Producto{
     }
     public function find($id)
     {
-        return $this->connection->runQuery('SELECT id,name,description, image, category, stock,price FROM products WHERE id = $1', [$id]);
+        return $this->connection->runQuery('SELECT id,name,description, image, stock,price, category FROM products WHERE id = $1', [$id]);
     }
 
-    public function create($name, $description, $image, $category, $stock,$price)
+    public function create($name, $description, $image,$stock,$price, $category)
     {
         $this->connection->runStatement('INSERT INTO products(
-    name,description,image,category, stock, price)
-    VALUES ($1,$2,$3,$4,$5,$6)', [$name,$description, $image, $category,
-     $stock,$price]);
+    name,description,image, stock, price,category)
+    VALUES ($1,$2,$3,$4,$5,$6)', [$name,$description, $image,
+     $stock,$price, $category]);
     }
 
     public function read()
@@ -25,11 +25,11 @@ class Producto{
         return $this->connection->runQuery('SELECT * FROM products ORDER BY id');
     }
 
-    public function update($id, $name,$description, $image, $category, $stock,$price)
+    public function update($id, $name,$description, $image, $stock,$price, $category)
     {
         $this->connection->runStatement('UPDATE products
-    SET name=$2,description= $3,image= $4, $category = $5, stock= $6,price= $7
-    WHERE id=$1', [$id, $name,$description, $image, $category, $stock,$price]);
+    SET name=$2,description= $3,image= $4, stock= $5,price= $6, $category = $5
+    WHERE id=$1', [$id, $name,$description, $image,$stock,$price, $category]);
     }
 
     public function delete($id)
