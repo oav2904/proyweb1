e59b00abaa8b3aa54c66bffc13a1b17879b1c01b
Oscar A <oscariasv@gmail.com>
2020-07-13 16:30:46 -0600

    se me olvidó algo jajajaja

--- page_1.php
+++ page_1.php
@@ -5,14 +5,14 @@ require_once './shared/db.php';
 $results = $user_model->sumusers();
 ?>
 <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Welcome <?= $results[0]['count']?>
-      </h1>
-      <p class="subtitle">
-        Page 1 content
-      </p>
-    </div>
+  <div class="container">
+    <h1 class="title">
+      Welcome <?= $results[0]['count'] ?>
+    </h1>
+    <p class="subtitle">
+      Page 1 content
+    </p>
+  </div>
 </section>
 
 
